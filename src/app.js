9c19cbdf67e23f282e7c09f8c24e534a21527da6
Calil-Silva <ka.renner@hotmail.com>
2021-10-13 22:45:05 -0300

    CRUD categories implemented

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const { Pool } = pg;
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 const connection = new Pool({
     user: 'bootcamp_role',
@@ -15,7 +15,28 @@ const connection = new Pool({
     port: 5432,
     database: 'boardcamp'
 });
+const namesObj = await connection.query('SELECT (name) FROM categories');
+const namesArr = namesObj.rows.map(({name}) => name);
 
-app.listen(4000);
+app.get('/categories', (req, res) => {
+    connection.query('SELECT * FROM categories;')
+        .then(r => res.send(r.rows))
+})
+
+app.post('/categories', (req, res) => {
+    const name = req.body.name;
+
+    if (name === '' || namesArr.includes(name)) {
+        res.sendStatus(400)
+    } else {
+        connection.query('INSERT INTO categories (name) VALUES ($1);', [name])
+        .then(r => res.sendStatus(201))
+        .catch(e => res.sendStatus(400));
+    }
+})
+
+app.listen(4000, () => {
+    console.log('Server listening on port 4000.');
+});
 
 
