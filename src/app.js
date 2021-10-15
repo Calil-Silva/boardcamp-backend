a1596f6bf11d80edfe7fff085c939ddd32c3651c
Calil-Silva <ka.renner@hotmail.com>
2021-10-15 12:22:10 -0300

    route customer partially done

--- src/app.js
+++ src/app.js
@@ -5,10 +5,6 @@ import joi from 'joi';
 
 const app = express();
 const { Pool } = pg;
-
-app.use(cors());
-app.use(express.json());
-
 const connection = new Pool({
     user: 'bootcamp_role',
     password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp',
@@ -19,13 +15,10 @@ const connection = new Pool({
 const categories = await connection.query('SELECT * FROM categories');
 const namesArr = categories.rows.map(({ name }) => name);
 const idsArr = categories.rows.map(({ id }) => id);
-const gamesSchema = joi.object({
-    name: joi.string(),
-    image: joi.string(),
-    stockTotal: joi.number().min(1),
-    categoryId: joi.number(),
-    pricePerDay: joi.number().min(1)
-})
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get('/categories', (req, res) => {
     connection.query('SELECT * FROM categories;')
@@ -44,13 +37,11 @@ app.post('/categories', (req, res) => {
     }
 })
 
-const handleCategoryGame = (categoryID) => {
-    return categories.rows.filter(({ id }) => id === categoryID)[0].name;
-}
-
 app.get('/games', (req, res) => {
-
     const filterParam = req.query.name;
+    const handleCategoryGame = (categoryID) => {
+        return categories.rows.filter(({ id }) => id === categoryID)[0].name;
+    }
 
     if (filterParam) {
         connection.query('SELECT * FROM games;')
@@ -62,13 +53,13 @@ app.get('/games', (req, res) => {
         connection.query('SELECT * FROM games;')
             .then(r => res.send(
                 r.rows.map(g => ({ ...g, categoryName: handleCategoryGame(g.categoryId) }),
-                )))
+                )
+            ))
     }
 
 })
 
 app.post('/games', async (req, res) => {
-
     const {
         name,
         image,
@@ -76,9 +67,14 @@ app.post('/games', async (req, res) => {
         categoryId,
         pricePerDay
     } = req.body
-
+    const gamesSchema = joi.object({
+        name: joi.string(),
+        image: joi.string(),
+        stockTotal: joi.number().min(1),
+        categoryId: joi.number(),
+        pricePerDay: joi.number().min(1)
+    })
     const { error } = gamesSchema.validate(req.body);
-
     const games = await connection.query('SELECT (name) FROM games;');
     const handleGamesName = games.rows.map(g => g.name);
 
@@ -89,10 +85,65 @@ app.post('/games', async (req, res) => {
     } else {
         connection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);`,
             [name, image, stockTotal, categoryId, pricePerDay])
-                .then(() => res.sendStatus(201))
+            .then(() => res.sendStatus(201));
     }
 })
 
+app.get('/customers', async (req, res) => {
+    const { cpf } = req.query;
+
+    if(cpf) {
+        const promise = await connection.query(`SELECT * FROM customers WHERE cpf LIKE '${cpf}%';`);
+        res.send(promise.rows);
+    } else {
+        const promise = await connection.query('SELECT * FROM customers;');
+        res.send(promise.rows);
+    }
+
+})
+
+app.get('/customers/:id', async (req, res) => {
+    const id = +req.params.id;
+    const customersArr = await connection.query('SELECT (id) FROM customers');
+    const customersIdsArr = customersArr.rows.map(({id}) => id);
+    console.log(customersIdsArr, id)
+
+        if(customersIdsArr.includes(id)) {
+            const customers = await connection.query('SELECT * FROM customers WHERE id = $1', [id]);
+            res.send(customers.rows[0]);
+        } else {
+            res.sendStatus(404);
+        }
+
+})
+
+app.post('/customers', async (req, res) => {
+    const {
+        name,
+        phone,
+        cpf,
+        birthday
+    } = req.body;
+    const customersSchema = joi.object({
+        name: joi.string(),
+        phone: joi.string().pattern(/^([0-9]{11})|([0-9]{10})$/),
+        cpf: joi.string().pattern(/^[0-9]{11}$/),
+        birthday: joi.string().pattern(/^\d{2}[./-]\d{2}[./-]\d{4}$/)
+    });
+    const { error } = customersSchema.validate(req.body);
+    const promise = await connection.query('SELECT * FROM customers;'); 
+    const customersCPF = promise.rows.map(c => c.cpf);
+
+    if(error) {
+        res.sendStatus(400);
+    } else if(customersCPF.includes(cpf)) {
+        res.sendStatus(409);
+    } else {
+        await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);', 
+            [name, phone, cpf, birthday]);
+        res.sendStatus(201);
+    }
+})
 
 app.listen(4000, () => {
     console.log('Server listening on port 4000.');
